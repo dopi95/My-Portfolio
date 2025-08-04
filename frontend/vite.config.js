3cc3eeba3c1bccf65b89605046be0c3f81a0e6eb
dopi95 <elyasyenealem@gmail.com>
2025-08-05 00:56:34 +0300

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/My-Portfolio/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure this matches Netlify's publish directory
+  }
 });
